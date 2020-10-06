4ae45615dcd37c3fb102d1fe0c022b74cb1bda7d
Gleidson Duarte <work.gleidsonduarte@hotmail.com>
2020-10-06 09:53:55 -0300

    Finalizando projeto utilizado no curso de composer da Alura

---
+++ Teste/Teste.php
@@ -0,0 +1,14 @@
+<?php
+
+class Teste
+{
+    public static function metodo()
+    {
+        echo "Teste" . PHP_EOL;
+    }
+
+    public static function metodo2()
+    {
+        echo "Teste 2" . PHP_EOL;
+    }
+}
---
+++ buscar-cursos.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+// Teste::metodo2();
+// exit();
+
+// exibeMensagem('Oi');
+// exit();
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br']);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo $curso . PHP_EOL;
+}
